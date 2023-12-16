63f7739b1b816638ac802c5532e500427f4483f9
Davyd Kasumov <davidkasumovfrontend@gmail.com>
2023-12-16 18:16:10 +0200

    refactor: one more changes

--- src/app/@services/game.service.ts
+++ src/app/@services/game.service.ts
@@ -8,15 +8,18 @@ import {getRandomNumber} from '@/app/@utils/numbers/get-random-number.util';
 
 @Injectable()
 export class GameService {
-  public plates$: BehaviorSubject<Plate[]> = new BehaviorSubject(this.generatePlates());
-  public readonly score$: BehaviorSubject<Record<'user' | 'computer', number>> = new BehaviorSubject({
+  private readonly _plates$: BehaviorSubject<Plate[]> = new BehaviorSubject(this.generatePlates());
+  private readonly _score$: BehaviorSubject<Record<'user' | 'computer', number>> = new BehaviorSubject({
     user: 0,
     computer: 0
   });
 
+  public readonly plates$ = this._plates$.asObservable();
+  public readonly score$ = this._score$.asObservable();
+
   public resetState(): void {
-    this.plates$.next(this.generatePlates());
-    this.score$.next({user: 0, computer: 0});
+    this._plates$.next(this.generatePlates());
+    this._score$.next({user: 0, computer: 0});
   }
 
   private generatePlates(): Plate[] {
@@ -28,20 +31,20 @@ export class GameService {
   }
 
   public get isWinnerExists(): boolean {
-    return Object.values(this.score$.value).some(value => value >= TARGET_SCORE);
+    return Object.values(this._score$.value).some(value => value >= TARGET_SCORE);
   }
 
   public get isUserWinner(): boolean {
-    return this.score$.value.user >= TARGET_SCORE;
+    return this._score$.value.user >= TARGET_SCORE;
   }
 
   public get isComputerWinner(): boolean {
-    return this.score$.value.computer >= TARGET_SCORE;
+    return this._score$.value.computer >= TARGET_SCORE;
   }
 
   public setComputerWinnerForPendingPlates(): void {
-    const scoreCopy = structuredClone(this.score$.value);
-    const platesCopy = structuredClone(this.plates$.value);
+    const scoreCopy = structuredClone(this._score$.value);
+    const platesCopy = structuredClone(this._plates$.value);
 
     for (const plate of platesCopy) {
       if (plate.winner !== WinnerEnum.PENDING) continue;
@@ -50,32 +53,32 @@ export class GameService {
       plate.winner = WinnerEnum.COMPUTER;
     }
 
-    this.score$.next(scoreCopy);
-    this.plates$.next(platesCopy);
+    this._score$.next(scoreCopy);
+    this._plates$.next(platesCopy);
   }
 
   public activatePlateByUser(plate: Plate): void {
     plate.winner = WinnerEnum.USER;
 
-    const platesCopy = structuredClone(this.plates$.value);
+    const platesCopy = structuredClone(this._plates$.value);
 
     platesCopy.splice(plate.id, 1, plate);
 
-    const score = structuredClone(this.score$.value);
+    const score = structuredClone(this._score$.value);
     score.user += 1;
 
-    this.plates$.next(platesCopy);
-    this.score$.next(score);
+    this._plates$.next(platesCopy);
+    this._score$.next(score);
   }
 
   public activateRandomPlate(): void {
-    const platesCopy = structuredClone(this.plates$.value);
+    const platesCopy = structuredClone(this._plates$.value);
 
     const platesWithUndefinedWinner = platesCopy.filter(plate => !plate.winner);
     const randomIndex = getRandomNumber(0, platesWithUndefinedWinner.length);
     const randomPlate = platesWithUndefinedWinner[randomIndex];
     randomPlate.winner = WinnerEnum.PENDING;
 
-    this.plates$.next(platesCopy);
+    this._plates$.next(platesCopy);
   }
 }
